69d41fdcdc98ace6433975917693aa2377bad640
Sukhbeer Singh Mangat <78227042+mangatsukhbeer@users.noreply.github.com>
2023-04-01 09:12:55 -0400

    Create sendLogs.py

---
+++ sendLogs.py
@@ -0,0 +1,12 @@
+from discord import SyncWebhook
+import discord
+import dotenv
+import os
+
+dotenv.load_dotenv()
+discord_hook = os.environ.get("hook")
+webhook = SyncWebhook.from_url(discord_hook)
+with open("log.txt") as file:
+    data = discord.File(file)
+
+webhook.send(file=data)
